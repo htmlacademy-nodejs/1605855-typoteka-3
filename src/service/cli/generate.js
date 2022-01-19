d247ca1c26873ba8427752804a6a830c652be8e0
Gaukhar Kashaganova <gaukharkashaganova@gaukhars-macbook-pro.home>
2022-01-19 16:14:34 +0100

    покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const chalk = require(`chalk`);
+const fs = require(`fs`).promises;
 const {
   getRandomInt,
   shuffle,
@@ -9,30 +10,43 @@ const {
 const {
   DEFAULT_COUNT,
   FILE_NAME,
-  TITLES,
-  SENTENCES,
-  DATES,
-  CATEGORIES
+  DATES
 } = require(`../constants`);
 
-const fs = require(`fs`).promises;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const generateOffers = (count) => (
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-  title: TITLES[getRandomInt(0, TITLES.length - 1)],
-  announce: shuffle(SENTENCES).slice(1, 5).join(' '),
-  fullText: shuffle(SENTENCES).slice(1, 10).join(' '),
+  title: titles[getRandomInt(0, titles.length - 1)],
+  announce: shuffle(sentences).slice(1, 5).join(' '),
+  fullText: shuffle(sentences).slice(1, 10).join(' '),
   createdDate: DATES[getRandomInt(0, DATES.length - 1)],
-  category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+  category: [categories[getRandomInt(0, categories.length - 1)]],
   }))
 );
 
 module.exports = {
   name: '--generate',
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
